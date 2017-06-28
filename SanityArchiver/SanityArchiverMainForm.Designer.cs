fd97aca66ade2748e44263f493e208582896c9cb
AdamKovacs <kovacs.adam.mate@gmail.com>
2017-06-28 13:32:08 +0200

    Basic funtionality added

---
+++ SanityArchiver/SanityArchiverMainForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace SanityArchiver
+{
+    partial class SanityArchiver
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SanityArchiver));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.FilesTreeView = new System.Windows.Forms.TreeView();
+            this.iconsImageList = new System.Windows.Forms.ImageList(this.components);
+            this.filesListView = new System.Windows.Forms.ListView();
+            this.fileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lastModified = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.path = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchLabel = new System.Windows.Forms.Label();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.goToPathButton = new System.Windows.Forms.Button();
+            this.optionsContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.goToFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.optionsContextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 45);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.FilesTreeView);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.filesListView);
+            this.splitContainer1.Size = new System.Drawing.Size(850, 453);
+            this.splitContainer1.SplitterDistance = 283;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // FilesTreeView
+            // 
+            this.FilesTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FilesTreeView.ImageIndex = 0;
+            this.FilesTreeView.ImageList = this.iconsImageList;
+            this.FilesTreeView.Location = new System.Drawing.Point(0, 0);
+            this.FilesTreeView.Name = "FilesTreeView";
+            this.FilesTreeView.SelectedImageIndex = 0;
+            this.FilesTreeView.Size = new System.Drawing.Size(283, 453);
+            this.FilesTreeView.TabIndex = 0;
+            this.FilesTreeView.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.FilesTreeView_NodeMouseClick);
+            // 
+            // iconsImageList
+            // 
+            this.iconsImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("iconsImageList.ImageStream")));
+            this.iconsImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.iconsImageList.Images.SetKeyName(0, "folder-icon-64x64.png");
+            this.iconsImageList.Images.SetKeyName(1, "free-vector-file-icon_101922_File_Icon.png");
+            // 
+            // filesListView
+            // 
+            this.filesListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileName,
+            this.type,
+            this.lastModified,
+            this.Size,
+            this.path});
+            this.filesListView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.filesListView.Location = new System.Drawing.Point(0, 0);
+            this.filesListView.Name = "filesListView";
+            this.filesListView.Size = new System.Drawing.Size(563, 453);
+            this.filesListView.SmallImageList = this.iconsImageList;
+            this.filesListView.TabIndex = 0;
+            this.filesListView.UseCompatibleStateImageBehavior = false;
+            this.filesListView.View = System.Windows.Forms.View.Details;
+            this.filesListView.SelectedIndexChanged += new System.EventHandler(this.filesListView_SelectedIndexChanged);
+            this.filesListView.DoubleClick += new System.EventHandler(this.filesListView_DoubleClick);
+            // 
+            // fileName
+            // 
+            this.fileName.Text = "Name";
+            // 
+            // type
+            // 
+            this.type.Text = "Type";
+            // 
+            // lastModified
+            // 
+            this.lastModified.Text = "Last Modified";
+            // 
+            // Size
+            // 
+            this.Size.Text = "Size";
+            // 
+            // path
+            // 
+            this.path.Text = "Path";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.AutoSize = true;
+            this.pathLabel.Location = new System.Drawing.Point(13, 13);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(32, 13);
+            this.pathLabel.TabIndex = 1;
+            this.pathLabel.Text = "Path:";
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(54, 10);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(404, 20);
+            this.pathTextBox.TabIndex = 2;
+            this.pathTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.pathTextBox_KeyPress);
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(591, 10);
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(157, 20);
+            this.searchTextBox.TabIndex = 3;
+            this.searchTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.searchTextBox_KeyPress);
+            // 
+            // searchLabel
+            // 
+            this.searchLabel.AutoSize = true;
+            this.searchLabel.Location = new System.Drawing.Point(545, 13);
+            this.searchLabel.Name = "searchLabel";
+            this.searchLabel.Size = new System.Drawing.Size(44, 13);
+            this.searchLabel.TabIndex = 4;
+            this.searchLabel.Text = "Search:";
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(763, 8);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // goToPathButton
+            // 
+            this.goToPathButton.Location = new System.Drawing.Point(464, 7);
+            this.goToPathButton.Name = "goToPathButton";
+            this.goToPathButton.Size = new System.Drawing.Size(75, 23);
+            this.goToPathButton.TabIndex = 6;
+            this.goToPathButton.Text = "Go to Path";
+            this.goToPathButton.UseVisualStyleBackColor = true;
+            this.goToPathButton.Click += new System.EventHandler(this.goToPathButton_Click);
+            // 
+            // optionsContextMenuStrip
+            // 
+            this.optionsContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.goToFolderToolStripMenuItem});
+            this.optionsContextMenuStrip.Name = "optionsContextMenuStrip";
+            this.optionsContextMenuStrip.Size = new System.Drawing.Size(140, 26);
+            // 
+            // goToFolderToolStripMenuItem
+            // 
+            this.goToFolderToolStripMenuItem.Name = "goToFolderToolStripMenuItem";
+            this.goToFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.goToFolderToolStripMenuItem.Text = "Go to Folder";
+            this.goToFolderToolStripMenuItem.Click += new System.EventHandler(this.goToFolderToolStripMenuItem_Click);
+            // 
+            // SanityArchiver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 498);
+            this.Controls.Add(this.goToPathButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchLabel);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "SanityArchiver";
+            this.Text = "SanityArchiver";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.optionsContextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TreeView FilesTreeView;
+        private System.Windows.Forms.ListView filesListView;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Label searchLabel;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button goToPathButton;
+        private System.Windows.Forms.ImageList iconsImageList;
+        private System.Windows.Forms.ContextMenuStrip optionsContextMenuStrip;
+        private System.Windows.Forms.ColumnHeader fileName;
+        private System.Windows.Forms.ColumnHeader type;
+        private System.Windows.Forms.ColumnHeader lastModified;
+        private System.Windows.Forms.ColumnHeader Size;
+        private System.Windows.Forms.ColumnHeader path;
+        private System.Windows.Forms.ToolStripMenuItem goToFolderToolStripMenuItem;
+    }
+}
+
---
+++ SanityArchiver/SanityArchiverMainForm.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SanityArchiver
+{
+    public partial class SanityArchiver : Form
+    {
+        static string filePath;
+        static ListViewItem selectedItem;
+
+        public SanityArchiver()
+        {
+            InitializeComponent();
+            PopulateTreeView(@"C:\");
+            PopulateTreeView(@"E:\");
+        }
+
+        private void PopulateTreeView(string destination)
+        {
+            TreeNode rootNode;
+            try
+            {
+                DirectoryInfo info = new DirectoryInfo(destination);
+                if (info.Exists)
+                {
+                    rootNode = new TreeNode(info.Name);
+                    rootNode.Tag = info;
+                    GetDirectories(info.GetDirectories(), rootNode);
+                    FilesTreeView.Nodes.Add(rootNode);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void GetDirectories(DirectoryInfo[] subDirs, TreeNode nodeToAddTo)
+        {
+            try
+            {
+                TreeNode aNode;
+                DirectoryInfo[] subSubDirs;
+                foreach (DirectoryInfo subDir in subDirs)
+                {
+                    aNode = new TreeNode(subDir.Name, 0, 0);
+                    aNode.Tag = subDir;
+                    aNode.ImageKey = "folder";
+                    subSubDirs = subDir.GetDirectories();
+                    if (subSubDirs.Length != 0)
+                    {
+                        GetDirectories(subSubDirs, aNode);
+                    }
+                    nodeToAddTo.Nodes.Add(aNode);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private void FilesTreeView_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            TreeNode newSelected = e.Node;
+            DirectoryInfo nodeDirInfo = (DirectoryInfo)newSelected.Tag;
+            refreshListView(nodeDirInfo);
+        }
+
+
+
+        private void refreshListView(DirectoryInfo newDir)
+        {
+            pathTextBox.Text = newDir.ToString();
+            filesListView.Items.Clear();
+            ListViewItem.ListViewSubItem[] subItems;
+            ListViewItem item = null;
+            foreach (DirectoryInfo dir in newDir.GetDirectories())
+            {
+                item = new ListViewItem(dir.Name, 0);
+                subItems = new ListViewItem.ListViewSubItem[]
+                          {new ListViewItem.ListViewSubItem(item, "Directory"),
+                          new ListViewItem.ListViewSubItem(item,
+                          dir.LastAccessTime.ToShortDateString()),
+                          new ListViewItem.ListViewSubItem(item, ""),
+                          new ListViewItem.ListViewSubItem(item, dir.FullName) };
+                item.SubItems.AddRange(subItems);
+                filesListView.Items.Add(item);
+            }
+            foreach (FileInfo file in newDir.GetFiles())
+            {
+                item = new ListViewItem(file.Name, 1);
+                subItems = new ListViewItem.ListViewSubItem[]
+                          { new ListViewItem.ListViewSubItem(item, "File"),
+                            new ListViewItem.ListViewSubItem(item,
+                            file.LastAccessTime.ToShortDateString()),
+                            new ListViewItem.ListViewSubItem(item, (file.Length/1024).ToString()+" MB"),
+                            new ListViewItem.ListViewSubItem(item, file.FullName)};
+
+                item.SubItems.AddRange(subItems);
+                filesListView.Items.Add(item);
+            }
+
+            filesListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+        }
+
+        private void goToFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            filesListView.Items.Clear();
+            DirectoryInfo newDir = new DirectoryInfo(filePath);
+            refreshListView(newDir);
+        }
+
+        private void filesListView_DoubleClick(object sender, EventArgs e)
+        {
+            if (selectedItem.SubItems[1].Text == "Directory")
+            {
+                DirectoryInfo newDir = new DirectoryInfo(filePath);
+                refreshListView(newDir);
+            }
+            else
+            {
+                Process.Start(filePath);
+            }
+        }
+
+        private void pathTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ((char)13))
+            {
+                try
+                {
+                    DirectoryInfo newDir = new DirectoryInfo(pathTextBox.Text);
+                    refreshListView(newDir);
+                }
+                catch (Exception f)
+                {
+                    Console.WriteLine(f.Message);
+                }
+
+            }
+        }
+
+        private void goToPathButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DirectoryInfo newDir = new DirectoryInfo(pathTextBox.Text);
+                refreshListView(newDir);
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine(f.Message);
+            }
+        }
+
+        private void searchTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ((char)13))
+            {
+                try
+                {
+                    string[] dirs = Directory.GetFiles(pathTextBox.Text, searchTextBox.Text);
+                    ListViewItem.ListViewSubItem[] subItems;
+                    ListViewItem item = null;
+                    filesListView.Items.Clear();
+
+                    foreach (string dir in dirs)
+                    {
+                        Console.WriteLine(dir);
+                        FileInfo file = new FileInfo(dir);
+                        //refreshListView(new DirectoryInfo(file.Directory.ToString()));
+
+                        item = new ListViewItem(file.Name, 1);
+                        subItems = new ListViewItem.ListViewSubItem[]
+                                    { new ListViewItem.ListViewSubItem(item, "File"),
+                        new ListViewItem.ListViewSubItem(item,
+                        file.LastAccessTime.ToShortDateString()),
+                        new ListViewItem.ListViewSubItem(item, (file.Length/1024).ToString()+" MB"),
+                        new ListViewItem.ListViewSubItem(item, file.FullName)};
+
+                        item.SubItems.AddRange(subItems);
+                        filesListView.Items.Add(item);
+
+
+
+                    }
+                    filesListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+                }
+                catch (Exception f)
+                {
+                    Console.WriteLine("The process failed: {0}", f.ToString());
+                }
+            }
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] dirs = Directory.GetFiles(pathTextBox.Text, searchTextBox.Text);
+                ListViewItem.ListViewSubItem[] subItems;
+                ListViewItem item = null;
+                filesListView.Items.Clear();
+
+                foreach (string dir in dirs)
+                {
+                    Console.WriteLine(dir);
+                    FileInfo file = new FileInfo(dir);
+                    //refreshListView(new DirectoryInfo(file.Directory.ToString()));
+
+                    item = new ListViewItem(file.Name, 1);
+                    subItems = new ListViewItem.ListViewSubItem[]
+                                { new ListViewItem.ListViewSubItem(item, "File"),
+                        new ListViewItem.ListViewSubItem(item,
+                        file.LastAccessTime.ToShortDateString()),
+                        new ListViewItem.ListViewSubItem(item, (file.Length/1024).ToString()+" MB"),
+                        new ListViewItem.ListViewSubItem(item, file.FullName)};
+
+                    item.SubItems.AddRange(subItems);
+                    filesListView.Items.Add(item);
+
+
+
+                }
+                filesListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            }
+            catch (Exception f)
+            {
+                Console.WriteLine("The process failed: {0}", f.ToString());
+            }
+        }
+
+        private void filesListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filePath = filesListView.FocusedItem.SubItems[4].Text;
+            selectedItem = filesListView.FocusedItem;
+        }
+    }
+}
